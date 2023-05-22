bb59b88e9a30656a72f0af4e6c01524a96b01810
kmkan <101231985+kmkan@users.noreply.github.com>
2023-05-22 10:14:22 -0600

    Hover state fixes

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const MENUITEMS = document.querySelectorAll('li.menu-item.special');
 const ARROWS = document.querySelectorAll('li.menu-item i');
 const BARS = document.querySelector('.bars');
 const NAV = document.querySelector('nav');
 const CANCEL = document.querySelector('.x');
 const OVERLAY = document.querySelector('.overlay');
+const MENUS = document.querySelectorAll('.menu-container');
 
+MENUS.forEach(item => item.addEventListener('mouseover', () => {
+    let listItem = item.children[0];
+    let childOne = listItem.children[0];
+    childOne.classList.toggle('fa-angle-down');
+    childOne.classList.toggle('fa-angle-up');
+}))
 
-MENUITEMS.forEach(item => item.addEventListener('click', () => {
-    let childOne = item.firstElementChild;
-    let childTwo = item.children[1];
-    childTwo.classList.toggle('active');
+MENUS.forEach(item => item.addEventListener('mouseout', () => {
+    let listItem = item.children[0];
+    let childOne = listItem.children[0];
     childOne.classList.toggle('fa-angle-down');
     childOne.classList.toggle('fa-angle-up');
 }))
@@ -31,11 +36,5 @@ window.addEventListener('resize', () => {
     }
 })
 
-// MENUITEMS.forEach(item => item.addEventListener('mouseout', () => {
-//     let childOne = item.firstElementChild;
-//     let childTwo = item.children[1];
-//     childTwo.classList.toggle('active');
-//     childOne.classList.toggle('fa-angle-down');
-//     childOne.classList.toggle('fa-angle-up');
-// }))
+
 
